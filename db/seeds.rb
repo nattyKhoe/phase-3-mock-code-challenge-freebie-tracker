ac3db06cedc6897114626b7f25319c2be4c98a61
Natasha <natasha.khoe@gmail.com>
2023-01-06 21:44:26 +1100

    establishing many-to-many relationship, have not done the testing.

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,11 @@ Dev.create(name: "Mr. Meseeks")
 Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
+Freebie.create(item_name: "Pencil", value: 3, dev_id: 1, company_id: 1)
+Freebie.create(item_name: "Pen", value: 3, dev_id: 2, company_id: 2)
+Freebie.create(item_name: "Ruler", value: 3, dev_id: 4, company_id: 4)
+Freebie.create(item_name: "Pencil Case", value: 3, dev_id: 3, company_id: 3)
+Freebie.create(item_name: "Keyboards", value: 3, dev_id: 1, company_id: 1)
 
 # ***************************************************************
 # * TODO: create freebies! Remember, a freebie belongs to a dev *
