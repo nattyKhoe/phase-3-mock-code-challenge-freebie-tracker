2c34bb2ec2eec596717daf20f1a835991a374337
Natasha <natasha.khoe@gmail.com>
2023-01-07 15:11:57 +1100

    done

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,4 +1,23 @@
 class Dev < ActiveRecord::Base
 has_many :freebies
 has_many :companies, through: :freebies
+
+    def received_one? (item_name)
+        # item_check = self.freebies.map do |freebie|
+        #     freebie.item_name == item_name
+        # end
+
+        # item_check.include?(true)
+
+        self.freebies.any? do |freebie|
+            freebie.item_name == item_name
+        end
+
+    end
+
+    def give_away(dev, freebie)
+        # self.freebies.delete(freebie)
+        # dev.freebies.push(freebie)
+        freebie.update(dev: dev) unless freebie.dev != self
+    end
 end
